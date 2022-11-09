b26db98d44b94d56fd5d9f30b49dea1217f0aa46
moris <morissheikman4@gmail.com>
2022-11-09 21:47:08 +0200

    commit

---
+++ src/Main.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        int w = scanner.nextInt();
+//        int e = scanner.nextInt();
+//        domo(w, e);
+        //  System.out.println("Hello friend please put your shakel!");
+        //  Scanner scanner = new Scanner(System.in);
+//        int[] inputs = new int[5];
+//        for (int i = 0; i < inputs.length; i++) {
+//           inputs[i] = scanner.nextInt();
+//       }
+//       int sum = 0;
+//       for (int number : inputs) {
+//           sum = sum + number;
+//       }
+//       System.out.println("first number");
+//       System.out.println(inputs[0]);
+//       System.out.println("second number");
+//        System.out.println(inputs[1]);
+//       System.out.println("all the numbers ");
+//       System.out.println(sum);
+//       System.out.println("avg of all numbers");
+//       System.out.println("avg  " + sum / inputs.length);
+//
+//
+//       for (int i=0; i<inputs.length;i++)
+//        if (inputs[i] >50 ) {
+//            System.out.println(" all the number +50 " + inputs[i]);
+//
+//    }
+        //System.out.println("please press money balance to convert shekl to dollar");
+        //Scanner scanner = new Scanner(System.in);
+        //double USD= 3.5260;
+        //double EUR = 3.6850;
+
+        //double input = scanner.nextDouble();
+        // while (input != 0) {
+        //        if (input == 1)
+        //    System.out.println("please enter a sum to change");
+        //   System.out.println("sum after change with Dollar " + input / USD);
+        // } else { (input == 2) ;
+//Scanner scanner = new Scanner(System.in);
+//int[] nums = new int[5];
+//for (int i = 0 ; i <5 ;i ++ ) {
+//    System.out.println("please enter number"  + (i+1)+ "numbers");
+//}
+//        Scanner scanner = new Scanner(System.in);
+//        int r = scanner.nextInt();
+//        int y = scanner.nextInt();
+//        sum(r,y);
+//    }
+//
+//    public static void sum(int x, int y) {
+//        System.out.println(x+" <> "+y);
+        int r = scanner.nextInt();
+        int t = scanner.nextInt();
+        double ans = div(r,t);
+        System.out.println("your avg is " + ans);
+    }
+
+    public static double div(double x,double y){
+      return (x/y) ;   }
+
+    //    public static void domo(int x,int y)
+//
+//    {
+//        System.out.println(x + "_____" + y);
+//
+//    }
+}
+
